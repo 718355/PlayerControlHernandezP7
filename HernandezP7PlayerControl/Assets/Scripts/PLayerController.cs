00daf32b5efaf9f03997cf7d43e3772b1e000dbd
718355 <718355@student.dusd.net>
2024-02-29 15:29:39 -0800

    Almost done with the game.

--- HernandezP7PlayerControl/Assets/Scripts/PLayerController.cs
+++ HernandezP7PlayerControl/Assets/Scripts/PLayerController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PLayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
+    // Start is called before the first frame update 
+    private float speed = 5.0f;
+    private float turnSpeed;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +19,13 @@ public class PLayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the Vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+
+        // We move the Vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the Vehicle 
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
